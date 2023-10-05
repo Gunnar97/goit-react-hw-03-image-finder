06699447f6a065290281161806f216ea8bd86951
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-10-05 14:48:51 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Button from 'components/Button/Button';
 import ImageGallery from 'components/ImageGallery/ImageGallery';
 import Loader from 'components/Loader/Loader';
-import Modal from 'components/Modal/Modal';
 import SearchBar from 'components/SearchBar/SearchBar';
 import React, { Component } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { getData } from 'helper/api';
 import { PER_PAGE } from 'utils/constants';
+import Modal from 'components/Modal/Modal';
 
 class App extends Component {
   state = {
